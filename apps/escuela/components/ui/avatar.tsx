fe9760b3e9529e5e659b06af14293f726623549d
Felipe Barros <felipe.barros@gmail.com>
2025-03-13 01:23:06 -0300

    use profile-dropdown

--- apps/escuela/components/ui/avatar.tsx
+++ apps/escuela/components/ui/avatar.tsx
@@ -1,13 +1,13 @@
 
 
-export const Avatar = ({ image }: { image: string | undefined}) => {
+export const Avatar = ({ image, size, alt }: { image: string | undefined, size: number, alt: string | undefined}) => {
   return(
     <>
     {
       image ? (
-        <img className="inline-block h-8 w-8 rounded-full" src={image} alt="" />
+        <img className={`w-${size} h-${size} bg-gray-300 rounded-full flex-shrink-0`} src={image} alt={ alt } />
       ) : (
-        <span className="inline-block h-8 w-8 rounded-full overflow-hidden bg-gray-100">
+        <span className={`inline-block w-${size} h-${size} rounded-full overflow-hidden bg-gray-100`}>
           <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
             <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
           </svg>
--- apps/escuela/components/ui/profile-dropdown.tsx
+++ apps/escuela/components/ui/profile-dropdown.tsx
@@ -1,22 +1,76 @@
+'use client'
+import { useEffect, useState } from 'react'
 import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/react'
 import { ChevronsUpDown } from 'lucide-react'
+import { useShallow } from 'zustand/react/shallow'
 
+import { useProfileStore } from '@repo/supabase/providers/user-provider'
+import { createClient } from '@repo/supabase/lib/client'
+import { Avatar } from './avatar'
 import { cx } from "@repo/ui/utils/cx"
 import { siteConfig } from '../../app/siteConfig'
 
+const useProfile = () => {
+  return useProfileStore(
+    useShallow((store) => ({
+      profile: store.profile,
+      setProfile: store.setProfile,
+    }))
+  )
+}
 
 export const ProfileDropdownMobile = () => {
+  const supabase = createClient()
+  const { profile, setProfile } = useProfile()
+  const [avatarUrl, setAvatarUrl] = useState<string | undefined>(undefined)
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  useEffect(() => {
+    const downloadImage = async (path: string) => {
+      const { data, error } = await supabase.storage
+        .from('avatars').download(path)
+
+      if (error) throw error
+
+      const url = URL.createObjectURL(data)
+      setAvatarUrl(url)
+    }
+
+    if (profile?.avatar) downloadImage(profile.avatar)
+  }, [profile, supabase])
+
+  useEffect(() => {
+    const getProfile = async () => {
+      const { data: { user } } = await supabase.auth.getUser()
+
+      if (user) {
+        const { data, error } = await supabase
+        .from('profiles')
+        .select(`full_name, avatar_url`)
+        .eq('id', user?.id)
+        .single()
+
+        if (error) throw error
+
+        if (data) setProfile({
+          id: user.id,
+          email: user.email,
+          full_name: data.full_name,
+          avatar: data.avatar_url
+        })
+      }
+    }
+
+    if (!profile.id) getProfile()
+  }, [profile, supabase])
+
   return(
     <>
       <Menu as="div" className="ml-3 relative">
         <div>
           <MenuButton className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
             <span className="sr-only">Open user menu</span>
-            <img
-              className="h-8 w-8 rounded-full"
-              src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
+            <Avatar image={avatarUrl} size={8} alt={ profile && profile.full_name } />
           </MenuButton>
         </div>
           <MenuItems className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 focus:outline-none">
@@ -47,6 +101,49 @@ export const ProfileDropdownMobile = () => {
 
 
 export const ProfileDropdownDesktop = () => {
+  const supabase = createClient()
+  const { profile, setProfile } = useProfile()
+  const [avatarUrl, setAvatarUrl] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    const downloadImage = async (path: string) => {
+      const { data, error } = await supabase.storage
+        .from('avatars').download(path)
+
+      if (error) throw error
+
+      const url = URL.createObjectURL(data)
+      setAvatarUrl(url)
+    }
+
+    if (profile?.avatar) downloadImage(profile.avatar)
+  }, [profile, supabase])
+
+  useEffect(() => {
+    const getProfile = async () => {
+      const { data: { user } } = await supabase.auth.getUser()
+
+      if (user) {
+        const { data, error } = await supabase
+        .from('profiles')
+        .select(`full_name, avatar_url`)
+        .eq('id', user?.id)
+        .single()
+
+        if (error) throw error
+
+        if (data) setProfile({
+          id: user.id,
+          email: user.email,
+          full_name: data.full_name,
+          avatar: data.avatar_url
+        })
+      }
+    }
+
+    if (!profile.id) getProfile()
+  }, [profile, supabase])
+
   return(
     <>
       <Menu as="div" className="px-3 mt-3 relative inline-block text-left">
@@ -54,13 +151,9 @@ export const ProfileDropdownDesktop = () => {
           <MenuButton className="group w-full bg-gray-100 rounded-md px-3.5 py-2 text-sm text-left font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-purple-500">
             <span className="flex w-full justify-between items-center">
               <span className="flex min-w-0 items-center justify-between space-x-3">
-                <img
-                  className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0"
-                  src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80"
-                  alt=""
-                />
+                <Avatar image={avatarUrl} size={10} alt={ profile && profile.full_name } />
                 <span className="flex-1 flex flex-col min-w-0">
-                  <span className="text-gray-900 text-sm font-medium truncate">Jessy Schwarz</span>
+                  <span className="text-gray-900 text-sm font-medium truncate">{ profile && profile.full_name }</span>
                   <span className="text-gray-500 text-sm truncate">@jessyschwarz</span>
                 </span>
               </span>
