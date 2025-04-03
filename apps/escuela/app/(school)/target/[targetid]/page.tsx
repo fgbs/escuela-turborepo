0556794a088a8f7d517acf28e15a409719322dea
Felipe Barros <felipe.barros@gmail.com>
2025-04-03 18:26:03 -0300

    add biblio-list component

--- apps/escuela/app/(school)/target/[targetid]/page.tsx
+++ apps/escuela/app/(school)/target/[targetid]/page.tsx
@@ -1,24 +1,18 @@
-import Link from "next/link";
 import { redirect } from "next/navigation";
-import { ExternalLink, Paperclip } from "lucide-react";
-import { SiYoutube } from '@icons-pack/react-simple-icons';
 
 import { createClient } from "@repo/supabase/lib/server";
 import { getTargetsByTargetGroupId } from '@repo/supabase/models/targets'
-import { getDocumentsByTargetId } from '@repo/supabase/models/bibliographies'
 import { BackButton } from "@repo/ui/components/back-button";
 import { ContentRender } from "@repo/ui/components/content-render";
-import { siteConfig } from "../../../siteConfig";
+import { BiblioList } from "@repo/ui/components/biblio-list";
 import { Sidebar } from "../../../../components/ui/sidebar";
 import { RoomButton } from "../../../../components/room-button";
-import { RecordButton } from "../../../../components/record-button";
 
 
 export default async function TargetPage({ params }: { params: { targetid: string }}) {
   const supabase = await createClient()
   const targetid = (await params).targetid
   let menu = null
-  let documents = []
 
   const {
     data: { user },
@@ -44,24 +38,6 @@ export default async function TargetPage({ params }: { params: { targetid: strin
       path: '/target',
       steps: levels
     }
-
-    documents = await getDocumentsByTargetId(targetid)
-  }
-
-  const getLink = (doc) => {
-    if (doc.type === 'document') {
-      return `${siteConfig.storage.media}/${doc.course_id}/${targetid}/${doc.id}.${doc.ext}`
-    }
-
-    if (doc.type === 'youtube') {
-      return `https://www.youtube.com/watch?v=${doc.filename}`
-    }
-
-    if (doc.type === 'link') {
-      return `${doc.filename}`
-    }
-
-    return '#'
   }
   
   return (
@@ -90,51 +66,12 @@ export default async function TargetPage({ params }: { params: { targetid: strin
               <ContentRender content={ data.content } />
             </div>
 
-            <div className="px-4 py-4 bg-white shadow overflow-hidden sm:rounded-lg">
+            <div className="px-4 py-4 col-span-1 bg-white shadow overflow-hidden sm:rounded-lg">
               <div className="text-sm font-medium text-gray-800">
                 Bibliografía
               </div>
 
-              <ul role="list" className="mt-4 divide-y divide-gray-200">
-                {
-                  documents.map((doc) => (
-                    <li key={doc.id}>
-                      <Link 
-                        href={getLink(doc)} 
-                        rel="noopener noreferrer"
-                        target="_blank"
-                        aria-label="Downlod"
-                        className="block hover:bg-gray-50"
-                      >
-                        <div className="px-2 py-2 sm:px-4">
-                          <div className="flex items-center justify-between">
-                            <p className="text-sm font-medium text-indigo-600 truncate">
-                              {doc.name}
-                            </p>
-                          </div>
-                          <div className="mt-2 sm:flex sm:justify-between">
-                            <div className="sm:flex">
-                              <p className="flex items-center text-sm text-gray-500">
-                                {doc.author}
-                              </p>
-                            </div>
-                            <div className="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                              {
-                                doc.type === 'document' 
-                                ? <Paperclip className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                : doc.type === 'youtube' 
-                                ? <SiYoutube className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                : <ExternalLink className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                              }
-                            </div>
-                          </div>
-                        </div>
-                      </Link>
-                    </li>  
-                  ))
-                }
-              </ul>
-
+              <BiblioList target={ targetid } />
             </div>
           </div>
 
---
+++ packages/supabase/src/lib/storage.ts
@@ -0,0 +1,10 @@
+
+export const storage = {
+  bucket: {
+    avatar: 'https://dabrdpdopssigtsyzrbx.supabase.co/storage/v1/object/public/avatars',
+    media: 'https://dabrdpdopssigtsyzrbx.supabase.co/storage/v1/object/public/media',
+    record: 'https://fgbs.nyc3.cdn.digitaloceanspaces.com/directus-tvp/recordings',
+  },
+}
+
+export type storage = typeof storage
