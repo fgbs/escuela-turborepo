afd2b2571211c38129149023d96282d8862bbd9f
Felipe Barros <felipe.barros@gmail.com>
2025-04-03 19:31:19 -0300

    fix room buttons logic

--- apps/escuela/components/room-button.tsx
+++ apps/escuela/components/room-button.tsx
@@ -6,10 +6,27 @@ import { useRouter } from 'next/navigation'
 import { createClient } from "@repo/supabase/lib/client"
 
 
+const getRecords = async(id: string) => {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase
+    .from('recordings')
+    .select(`id`)
+    .eq('room_id', id)
+    .eq('default', true)
+    .maybeSingle()
+
+    if (error) throw error
+
+    return data?.id
+}
+
 export const RoomButton = ({ target }: { target: string }) => {
   const [loading, setLoading] = useState(true)
-  const [disabled, setDisabled] = useState(true)
-  const [room, setRoom] = useState()
+  const [roomDisabled, setRoomDisabled] = useState(true)
+  const [recordDisabled, setRecordDisabled] = useState(true)
+  const [room, setRoom] = useState(null)
+  const [record, setRecord] = useState(null)
   const router = useRouter()
 
   useEffect(() => {
@@ -18,17 +35,29 @@ export const RoomButton = ({ target }: { target: string }) => {
 
       const { data, error } = await supabase
         .from('rooms')
-        .select(`id`)
+        .select(`id, active, status`)
         .eq('target_id', id)
-        .eq('active', true)
         .maybeSingle()
 
       if (error) throw error
 
       setRoom(data)
-      console.log('room', data)
 
-      if(data) setDisabled(false)
+      if (data?.active && data?.status === 'open') {
+        setRoomDisabled(false)
+      }
+
+      if (data?.active && data?.status === 'ended') {
+        setRoomDisabled(true)
+
+        const rec = await getRecords(data?.id)
+        console.log('room', data, 'rec', rec)
+        if (rec) {
+          setRecordDisabled(false)
+          setRecord(rec)
+        }
+
+      }
 
       setLoading(false)
     }
@@ -41,8 +70,8 @@ export const RoomButton = ({ target }: { target: string }) => {
       <button
         type="button"
         className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:opacity-25 sm:order-1 sm:ml-3"
-        disabled={disabled}
-        aria-disabled={disabled}
+        disabled={roomDisabled}
+        aria-disabled={roomDisabled}
         onClick={() => router.push(`/room/${room?.id}`)}
       >
         {
@@ -54,8 +83,8 @@ export const RoomButton = ({ target }: { target: string }) => {
       <button
         type="button"
         className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 disabled:opacity-25 sm:order-1 sm:ml-3"
-        disabled={disabled}
-        aria-disabled={disabled}
+        disabled={recordDisabled}
+        aria-disabled={recordDisabled}
         onClick={() => router.push(`/record/${record?.id}`)}
       >
         {
