5c07e14555ba61c09e01826bed7dee6cbf3dea6b
Felipe Barros <felipe.barros@gmail.com>
2025-04-07 00:13:32 -0400

    middleware corst preflight

--- apps/escuela/middleware.ts
+++ apps/escuela/middleware.ts
@@ -1,8 +1,39 @@
-import { type NextRequest } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 import { updateSession } from '@repo/supabase/lib/middleware'
 
+const allowedOrigins = ['https://escuelatvp.cl', 'https://aula.escuelatvp.cl']
+ 
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
 
 export async function middleware(request: NextRequest) {
+  // Check the origin from the request
+  const origin = request.headers.get('origin') ?? ''
+  const isAllowedOrigin = allowedOrigins.includes(origin)
+
+  // Handle preflighted requests
+  const isPreflight = request.method === 'OPTIONS'
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+      ...corsOptions,
+    }
+    return NextResponse.json({}, { headers: preflightHeaders })
+  }
+ 
+  const requestHeaders = new Headers(request.headers);
+
+  if (isAllowedOrigin) {
+    requestHeaders.set('Access-Control-Allow-Origin', origin)
+  }
+ 
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    requestHeaders.set(key, value)
+  })
+
   // update user's auth session
   return await updateSession(request)
 }
--- apps/escuela/next.config.ts
+++ apps/escuela/next.config.ts
@@ -15,27 +15,6 @@ const nextConfig: NextConfig = {
       hostname: 'images.unsplash.com',
     }]
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
   transpilePackages: [
     "@repo/ui",
     "@repo/supabase",
