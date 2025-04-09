7b35f568f841a10299cba6412bf5e48e7ac0f11f
Felipe Barros <felipe.barros@gmail.com>
2025-04-09 15:21:50 -0400

    remove recording render

--- apps/escuela/app/(school)/record/[roomid]/page.tsx
+++ apps/escuela/app/(school)/record/[roomid]/page.tsx
@@ -1,14 +1,20 @@
 import { redirect } from "next/navigation";
+import '@vidstack/react/player/styles/default/theme.css';
+import '@vidstack/react/player/styles/default/layouts/video.css';
+import { MediaPlayer, MediaProvider } from '@vidstack/react'
+import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default'
 
 import { createClient } from '@repo/supabase/lib/server'
-import { RecordingRender } from "./recording-render";
 import { BackButton } from "@repo/ui/components/back-button";
+import { EmptyState } from "@repo/ui/components/empty-state";
 import { Sidebar } from "../../../../components/ui/sidebar";
+import { siteConfig } from '../../../siteConfig';
 
 
 export default async function TargetRecordPage({ params }: { params: { roomid: string }}) {
   const supabase = await createClient()
   const roomid = (await params).roomid
+  let media = null
 
   const {
     data: { user },
@@ -20,11 +26,26 @@ export default async function TargetRecordPage({ params }: { params: { roomid: s
 
   const { data, error } = await supabase
     .from('rooms')
-    .select('id, room_name, status, course_id')
+    .select(`
+      id, room_name, status, course_id,
+      recordings(id, filename, content_type)`)
     .eq('id', roomid)
+    .eq('recordings.default', true)
     .single()
 
   if (error) throw error
+
+  if (data) {
+    const { recordings } = data
+
+    if (recordings.length > 0) {
+      media = {
+        src: `${siteConfig.storage.record}/${data.course_id}/${data.id}/${recordings[0].filename}`,
+        type: `${recordings[0].content_type}`,
+      }  
+    }
+    // STORAGE_URL / course_id / [room_id] / [recording_id] / filename
+  }
 
   return(
     <>
@@ -44,7 +65,22 @@ export default async function TargetRecordPage({ params }: { params: { roomid: s
         </div>
 
         <div className="container mx-auto px-4 pt-4 sm:px-6 sm:pt-6 lg:px-8 lg:pt-8">
-          <RecordingRender room={ data } />
+          {
+            media ? (
+              <MediaPlayer 
+                title={ data.room_name || '' } 
+                src={ media.src }
+                crossOrigin
+                playsInline
+                autoPlay
+              >
+                <MediaProvider />
+                <DefaultVideoLayout icons={defaultLayoutIcons} />
+              </MediaPlayer>
+            ) : (
+              <EmptyState title={'Grabación'} subtitle={'No hay grabaciones disponibles.'} />
+            )
+          }
         </div>
       </Sidebar>
     </>
